f5e53f80cef82cb5816211093ddfdbce2e7874f2
iOS Warrior <sashastronk1@gmail.com>
2020-04-22 16:59:06 +0600

    Initial Commit

---
+++ CustomPickerView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomPickerView
+//
+//  Created by Stronk on 22.04.2020.
+//  Copyright © 2020 Practice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
